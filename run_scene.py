5bf2eabb4a69d96f34c186f1acf12cd096bcbf2d
AnhTu18294 <tu.na.1821994@gmail.com>
2017-03-06 12:28:50 +0100

    clean code

--- run_scene.py
+++ run_scene.py
@@ -13,20 +13,8 @@ fpath_design = 'models_places/deploy_resnet152_places365.prototxt'
 fpath_weights = 'models_places/resnet152_places365.caffemodel'
 fpath_labels = 'resources/labels.pkl'
 
-def predictions_scene(fpath_design, fpath_weights, fpath_labels, im):
-	# initilaize net
-	net = caffe.Net(fpath_design, fpath_weights, caffe.TEST)
-
-	# load input and configure preprocessing
-	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-	transformer.set_mean('data', np.load('/root/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))  # TODO - remove hardcoded path
-	transformer.set_transpose('data', (2,0,1))
-	transformer.set_channel_swap('data', (2,1,0))
-	transformer.set_raw_scale('data', 255.0)
-
-	# since we classify only one image, we change batch size from 10 to 1
-	net.blobs['data'].reshape(1,3,224,224)
-
+# predictions function
+def predictions_scene(net, im):
 	# load the image in the data layer
 	net.blobs['data'].data[...] = transformer.preprocess('data', im)
 
@@ -34,46 +22,39 @@ def predictions_scene(fpath_design, fpath_weights, fpath_labels, im):
 	out = net.forward()
 
 	# get all of probability accuracy for each image
-
 	return out["prob"].astype(float)
 
+# initilaize net
+net = caffe.Net(fpath_design, fpath_weights, caffe.TEST)
 
+# load input and configure preprocessing
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', np.load('/root/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))  # TODO - remove hardcoded path
+transformer.set_transpose('data', (2,0,1))
+transformer.set_channel_swap('data', (2,1,0))
+transformer.set_raw_scale('data', 255.0)
 
+# since we classify only one image, we change batch size from 10 to 1
+net.blobs['data'].reshape(1,3,224,224)
 
+f_out = open(fpath_outputs + 'predictions', 'w')
 
-f_out1 = open(fpath_outputs + 'predictions', 'w')
-
-# f_out2 = open(fpath_outputs + 'dumpfile_predictions', 'w')
-
-# result = {}
-t1 = 0
 with open(fpath_index, 'r') as f_in:
 	t1 = time.time()
 	image_index = f_in.readline()
 	index = 0
 	while image_index:
-		index = index + 1
-		# print '\n\n\n\nImage {} in processing ..... \n\n\n\n'.format(index)
-		if(index == 5):
-			print '\n\n\n\n time : {}'.format(time.time() - t1)
-			# time.sleep(3)
-			break
+		index += 1
+		if((index%1000) == 0):
+			print 'Image {} in processing .....'.format(index)
 		image_index = image_index.replace('\n', '')
 		image_file_path = fpath_data + image_index
 		im = caffe.io.load_image(image_file_path)
-
-		f_out1.write(image_index + ':\n')
-		probs = predictions_scene(fpath_design, fpath_weights, fpath_labels, im)
-		f_out1.write(str(probs))
+		f_out.write(image_index + ':\n')
+		probs = predictions_scene(net, im)
+		f_out.write(str(probs))
 		image_index = f_in.readline()
 
-f_out1.close()
-
-# pickle.dump(result, f_out2)
-# f_out2.close()
+f_out.close()
 
 print 'done!'
-
-# labels = pickle.load(f)
-# with open(fpath_outputs + 'labels', 'w') as f_out2:
-# 	f_out2.write(labels)
--- test.py
+++ test.py
@@ -14,20 +14,8 @@ fpath_design = '/home/anhtu/Desktop/places365/docker/models_places/deploy_alexne
 fpath_weights = '/home/anhtu/Desktop/places365/docker/models_places/alexnet_places365.caffemodel'
 fpath_labels = '/home/anhtu/Desktop/places365/docker/resources/labels.pkl'
 
-def predictions_scene(fpath_design, fpath_weights, fpath_labels, im, file_prediction):
-	# initilaize net
-	net = caffe.Net(fpath_design, fpath_weights, caffe.TEST)
-
-	# load input and configure preprocessing
-	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-	transformer.set_mean('data', np.load('/root/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))  # TODO - remove hardcoded path
-	transformer.set_transpose('data', (2,0,1))
-	transformer.set_channel_swap('data', (2,1,0))
-	transformer.set_raw_scale('data', 255.0)
-
-	# since we classify only one image, we change batch size from 10 to 1
-	net.blobs['data'].reshape(1,3,227,227)
-
+# predictions function
+def predictions_scene(net, im):	
 	# load the image in the data layer
 	net.blobs['data'].data[...] = transformer.preprocess('data', im)
 
@@ -35,34 +23,37 @@ def predictions_scene(fpath_design, fpath_weights, fpath_labels, im, file_predic
 	out = net.forward()
 
 	# get all of probability accuracy for each image
-
-	# out["prob"].tofile(f_out1, sep=" , ", format="%f")
-	
 	return out["prob"].astype(float)
 
+# initilaize net
+net = caffe.Net(fpath_design, fpath_weights, caffe.TEST)
 
+# load input and configure preprocessing
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', np.load('/root/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))  # TODO - remove hardcoded path
+transformer.set_transpose('data', (2,0,1))
+transformer.set_channel_swap('data', (2,1,0))
+transformer.set_raw_scale('data', 255.0)
 
-
+# since we classify only one image, we change batch size from 10 to 1
+net.blobs['data'].reshape(1,3,227,227)
 
 f_out1 = open(fpath_outputs + 'predictions', 'w')
 # f_out3 = open(fpath_outputs + 'dumpfile_predictions', 'w')
-t1 = 0
-t2 = 0
+
 with open(fpath_index, 'r') as f_in:
 	t1 = time.time()
 	image_index = f_in.readline()
 	index = 0
 	while image_index:
-		index = index + 1
-		print '\n\n\n\nImage {} in processing .....\n\n\n\n'.format(index)
-		# if(index == 2):
-		# 	time.sleep(3)
+		index += 1
+		if((index%2) == 0):
+			print 'Image {} in processing .....'.format(index)
 		image_index = image_index.replace('\n', '')
 		image_file_path = fpath_data + image_index
 		im = caffe.io.load_image(image_file_path)
-
 		f_out1.write(image_index + ':\n')
-		probs = predictions_scene(fpath_design, fpath_weights, fpath_labels, im, f_out1)
+		probs = predictions_scene(net, im)
 		f_out1.write(str(probs) + '\n')
 		image_index = f_in.readline()
 f_out1.close()
