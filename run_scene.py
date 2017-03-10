4c926a0c2a9c2171ae3245b59a3cca5781f10d2b
AnhTu18294 <tu.na.1821994@gmail.com>
2017-03-10 09:55:15 +0100

    change to get prop using python script

--- run_scene.py
+++ run_scene.py
@@ -5,60 +5,32 @@ import pickle
 import time
 
 caffe.set_mode_gpu() 
-#path to index and image data files:
-# fpath_index = '/video/trecvid/sin15/2016t/tshots/keylist1.txt'
-# fpath_data = '/video/trecvid/sin15/2016t/jpg/'
-# fpath_outputs = 'outputs/'
+
+batch_size = 3
+num_images = 25
+height = 224
+width = 224
+dim_feature = 3
 
 # # fetch pretrained models
 fpath_design = 'models_places/deploy_resnet152_places365_1.prototxt'
 fpath_weights = 'models_places/resnet152_places365.caffemodel'
 fpath_labels = 'resources/labels.pkl'
 
-# predictions function
-# def predictions_scene(net, im):
-# 	# load the image in the data layer
-# 	net.blobs['data'].data[...] = transformer.preprocess('data', im)
-
-# 	#compute
-# 	out = net.forward()
-
-# 	# get all of probability accuracy for each image
-# 	return out["prob"]
+num_batchs = num_images/batch_size
+rest_images = num_images - num_batchs*batch_size
 
 # initilaize net
 net = caffe.Net(fpath_design, fpath_weights, caffe.TEST)
+net.blobs['data'].reshape(batch_size,dim_feature,height,width)
 
-# load input and configure preprocessing
-# transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-# transformer.set_mean('data', np.load('/home/mrim/nguyenan/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))  # TODO - remove hardcoded path
-# transformer.set_transpose('data', (2,0,1))
-# transformer.set_channel_swap('data', (2,1,0))
-# transformer.set_raw_scale('data', 255.0)
+fout_prop = open('outputs/prop.bin', 'w')
 
-# since we classify only one image, we change batch size from 10 to 1
-# net.blobs['data'].reshape(1,3,224,224)
+for i in range (0, num_batchs):
+	net.forward()["prob"].tofile(fout_prop, '')
 
-# f_out = open(fpath_outputs + 'predictions', 'w')
-
-# with open(fpath_index, 'r') as f_in:
-# 	t1 = time.time()
-# 	image_index = f_in.readline()
-# 	index = 0
-# 	while image_index:
-# 		index += 1
-# 		if((index%1000) == 0):
-# 			print 'Image {} in processing .....'.format(index)
-# 		image_index = image_index.replace('\n', '')
-# 		image_file_path = fpath_data + image_index
-# 		im = caffe.io.load_image(image_file_path)
-# 		probs = predictions_scene(net, im)
-# 		probs.tofile(f_out, '')
-# 		# f_out.write(image_index + ':\n'+ str(probs))
-# 		image_index = f_in.readline()
-
-# f_out.close()
-
-print net.forward()["prob"].size
+if(rest_images != 0):
+	net.blobs['data'].reshape(rest_images,dim_feature,height,width)
+	net.forward()["prop"].tofile(fout_prop, '')
 
 print 'done!'
