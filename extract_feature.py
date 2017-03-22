916d79e42751d33e44987a56ea04fd43d88e925c
AnhTu18294 <tu.na.1821994@gmail.com>
2017-03-22 10:25:54 +0100

    clean code

--- extract_feature.py
+++ extract_feature.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys, os
-os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda-8.0/lib64/'
+# os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda-8.0/lib64/'
+sys.path.append('/usr/local/cuda-8.0/lib64/')
 sys.path.append('/opt/caffe/python/')
 import optparse
 import caffe
