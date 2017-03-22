4091d37dbcbf53f9c3d5f4f054ecc8001de730d9
AnhTu18294 <tu.na.1821994@gmail.com>
2017-03-22 10:00:40 +0100

    clean code

--- extract_feature.py
+++ extract_feature.py
@@ -1,5 +1,7 @@
 import numpy as np
-import sys, getopt, os
+import sys, os
+sys.path.append('/opt/caffe/python/')
+os.system('export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/:$LD_LIBRARY_PATH')
 import optparse
 import caffe
 from caffe import layers as L
